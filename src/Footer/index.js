ab7cadf3812e9a60cfb96160efd14bab3ecce6bc
Marta Zebrowska-Giers <zeberita@gmail.com>
2023-01-25 12:16:20 +0100

    footer changed

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -2,9 +2,7 @@ import "./style.css";
 
 const Footer = (props) => (
     <footer className="footer">
-      <p>Aktualne kursy walut na dzień 27.10.2022 według NBP:
-        EURO (EUR): 4.7504 Dolar amerykański (USD): 4.7216 Frank szwajcarski
-        (CHF): 4.7798 Funt brytyjski (GBP): 5.4728
+      <p>Kurs z dnia 24.01.2023.
       </p>
       </footer>
 );
