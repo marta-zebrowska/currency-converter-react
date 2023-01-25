abc8a2c58a47a213712b298d184d7627f91dd347
Marta Zebrowska-Giers <zeberita@gmail.com>
2023-01-25 12:16:04 +0100

    currencies added

---
+++ src/currencies.js
@@ -0,0 +1,25 @@
+export const currencies = [
+{
+    short: "EUR",
+    name: "Euro",
+    rate: 4.71
+},
+
+{
+    short: "USD",
+    name: "Dolar amerykański",
+    rate: 4.33
+},
+
+{
+    short: "CHF",
+    name: "Frank szwajcarski",
+    rate: 4.70
+},
+
+{
+    short: "GBP",
+    name: "Funt brytyjski",
+    rate: 5.34
+},
+];
