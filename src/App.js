870f9328901800571312df190b55ed94f0a789c0
Marta Zebrowska-Giers <zeberita@gmail.com>
2023-01-26 12:03:38 +0100

    Header and Result deleted

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import Container from "./Container";
-import Header from "./Header"; 
-import Form from "./Form";
-import Result from "./Result";
+import { Form } from "./Form";
 import Footer from "./Footer";
 
 function App() {
   return (
    <Container>
-    <Header />
     <Form />
-    <Result />
     <Footer />
    </Container> 
     );
