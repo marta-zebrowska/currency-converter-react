a09ad4f481c1246f3a9d02061b0d33acb4fdfbcb
Marta Zebrowska-Giers <zeberita@gmail.com>
2023-02-01 14:07:02 +0100

    year deleted from Clock

--- src/Form/Clock/index.js
+++ src/Form/Clock/index.js
@@ -20,7 +20,6 @@ return (
             weekday: "long",
             day: "numeric",
             month: "long",
-            year: "numeric",
             hour: "2-digit",
             minute: "2-digit",
             second: "2-digit",
