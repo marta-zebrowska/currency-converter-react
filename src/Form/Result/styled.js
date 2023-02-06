1d8d1e536303e46c6052a1b89d1d949614ac2fba
Marta Zebrowska-Giers <zeberita@gmail.com>
2023-02-06 10:04:09 +0100

    style components added

---
+++ src/Form/Result/styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const StyledResult = styled.div`
+    font-size: 20px;
+    padding: 10px;
+    margin-left: 5px;
+    color: #39206b;
+    text-align: center;
+`;
