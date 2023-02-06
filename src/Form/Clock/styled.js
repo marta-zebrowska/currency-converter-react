64bac81539455df9e3d4f5dac2c467ce7e730450
Marta Zebrowska-Giers <zeberita@gmail.com>
2023-02-06 10:03:59 +0100

    style components added

---
+++ src/Form/Clock/styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const StyledClock = styled.div`
+    display: flex;
+    flex-direction: row-reverse;
+    align-items: flex-start;
+    font-family: 'Courier Prime', monospace;
+    font-size: 14px;
+`;
