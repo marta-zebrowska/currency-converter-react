e8deeafb4a3df3376574795e1e71586944451d4d
Marta Zebrowska-Giers <zeberita@gmail.com>
2023-02-09 10:48:01 +0100

    breakpoint deleted

--- src/theme.js
+++ src/theme.js
@@ -1,12 +1,9 @@
 export const theme = {
-    color: {
-        deepKoamaru: "#39206b",
-        musselWhite: "#F7F7F9",
-        pastelGray: "#c4c6d4",
-        black: "#000",
-        deepMauve: "#dc83dc",
-      },
-    breakpoint: {
-        mobile: 767,
-    },
+  color: {
+    deepKoamaru: "#39206b",
+    musselWhite: "#F7F7F9",
+    pastelGray: "#c4c6d4",
+    black: "#000",
+    deepMauve: "#dc83dc",
+  },
 };
