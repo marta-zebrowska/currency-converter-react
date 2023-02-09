da8b4beab0faa3fa5a6265f065bf33d5d2f01912
Marta Zebrowska-Giers <zeberita@gmail.com>
2023-02-09 11:23:13 +0100

    useCurrentDate hook added

--- src/Form/Clock/index.js
+++ src/Form/Clock/index.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useCurrentDate } from "./useCurrentDate";
 import { StyledClock } from "./styled";
 
 const formattedDate = (date) =>
@@ -12,15 +12,7 @@ const formattedDate = (date) =>
   });
 
 export const Clock = () => {
-  const [date, setDate] = useState(new Date());
-
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setDate(new Date());
-    }, 1000);
-
-    return () => clearInterval(intervalId);
-  }, []);
+  const date = useCurrentDate();
 
   return (
   <StyledClock>
