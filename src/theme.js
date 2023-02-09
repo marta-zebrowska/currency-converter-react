488f59b53b6d321c17e6173b2fa1cfd34ee54525
Marta Zebrowska-Giers <zeberita@gmail.com>
2023-02-09 10:03:01 +0100

    theme created

---
+++ src/theme.js
@@ -0,0 +1,11 @@
+export const theme = {
+    color: {
+        deepKoamaru: "#39206b",
+        musselWhite: "#f0e2de",
+        black: "#aaa",
+        deepMauve: "#dc83dc",
+      },
+    breakpoint: {
+        mobile: 767,
+    },
+};
