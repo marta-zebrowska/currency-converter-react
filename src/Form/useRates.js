ea5346f5ca70f35c735c6ae8418a08afe604d4b6
Marta Zebrowska-Giers <zeberita@gmail.com>
2023-02-13 13:23:25 +0100

    useRates hook created

---
+++ src/Form/useRates.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+export const useRates = () => {
+  const [rates, setRates] = useState({
+    state: "loading",
+  });
+
+  useEffect(() => {
+    const getRates = async () => {
+      try {
+        const response = await axios.get(
+          "https://api.exchangerate.host/latest?base=PLN"
+        );
+
+        setRates({
+          state: "working",
+          rates: response.data.rates,
+          date: response.data.date,
+        });
+      } catch {
+        setRates({
+          state: "error",
+        });
+      }
+    };
+
+    setTimeout(getRates, 2000);
+  }, []);
+
+  return rates;
+};
