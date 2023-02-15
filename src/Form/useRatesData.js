83ed8a50f3c0ce2270f5d5e04bde6247b6063329
Marta Zebrowska-Giers <zeberita@gmail.com>
2023-02-15 10:37:50 +0100

    useRates changed to useRatesData

--- src/Form/useRatesData.js
+++ src/Form/useRatesData.js
@@ -1,32 +1,32 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 
-export const useRates = () => {
-  const [rates, setRates] = useState({
+export const useRatesData = () => {
+  const [ratesData, setRatesData] = useState({
     state: "loading",
   });
 
   useEffect(() => {
-    const getRates = async () => {
+    const getRatesData = async () => {
       try {
         const response = await axios.get(
           "https://api.exchangerate.host/latest?base=PLN"
         );
 
-        setRates({
+        setRatesData({
           state: "success",
           rates: response.data.rates,
           date: response.data.date,
         });
       } catch {
-        setRates({
+        setRatesData({
           state: "error",
         });
       }
     };
 
-    setTimeout(getRates, 2000);
+    setTimeout(getRatesData, 2000);
   }, []);
 
-  return rates;
+  return ratesData;
 };
