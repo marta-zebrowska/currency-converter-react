743c959de49197449d18ec3c2117e638e16de012
Marta Zebrowska-Giers <zeberita@gmail.com>
2023-02-15 10:43:24 +0100

    targetAmount changed to resultValue

--- src/Form/Result/index.js
+++ src/Form/Result/index.js
@@ -2,12 +2,14 @@ import "./styled.js";
 import { StyledResult } from "./styled";
 
 export const Result = ({ result }) => (
-    <StyledResult>
-      {result !== "" && (
-                <>
-                    {result.amount}&nbsp;PLN&nbsp;={" "}
-                    <strong>{result.targetAmount.toFixed(2)}&nbsp;{result.currency}</strong>
-                </>
-            )}
+  <StyledResult>
+    {result !== "" && (
+      <>
+        {result.amount}&nbsp;PLN&nbsp;={" "}
+        <strong>
+          {result.resultValue.toFixed(2)}&nbsp;{result.currency}
+        </strong>
+      </>
+    )}
   </StyledResult>
-)
+);
