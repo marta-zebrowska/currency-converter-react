e310c738ca52da77073422d9d64c659013c1636c
Marta Zebrowska-Giers <zeberita@gmail.com>
2023-02-15 10:41:47 +0100

    sourceAmount changed to amount

--- src/Form/Result/index.js
+++ src/Form/Result/index.js
@@ -5,7 +5,7 @@ export const Result = ({ result }) => (
     <StyledResult>
       {result !== "" && (
                 <>
-                    {result.sourceAmount.toFixed(2)}&nbsp;PLN&nbsp;={" "}
+                    {result.amount}&nbsp;PLN&nbsp;={" "}
                     <strong>{result.targetAmount.toFixed(2)}&nbsp;{result.currency}</strong>
                 </>
             )}
