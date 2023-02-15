3deb2ebc136f4c2d3b60ad20fbd737f8154f0a6b
Marta Zebrowska-Giers <zeberita@gmail.com>
2023-02-15 10:51:07 +0100

    code formatted

--- src/Form/index.js
+++ src/Form/index.js
@@ -2,7 +2,16 @@ import { currencies } from "../currencies";
 import { useState } from "react";
 import { Result } from "./Result";
 import { Clock } from "./Clock";
-import { StyledFieldset, Legend, Label, Field, StyledButton, Loading, StyledError, Info } from "./styled";
+import {
+  StyledFieldset,
+  Legend,
+  Label,
+  Field,
+  StyledButton,
+  Loading,
+  StyledError,
+  Info,
+} from "./styled";
 import { useRatesData } from "./useRatesData";
 
 const Form = () => {
@@ -28,67 +37,61 @@ const Form = () => {
 
   return (
     <form onSubmit={onFormSubmit}>
-      {ratesData.state === "loading"
-                ? (
-                    <Loading>
-                        Uno momento por favor... <br /> Właśnie ładuję kursy z Europejskiego Banku Centralnego...
-                    </Loading>
-                )
-                :
-                ratesData.state === "error" ? (
-                    <StyledError>
-                        Ups... coś poszło nie tak 😐 <br />
-                        Sprawdź czy masz połączenie z internetem i odśwież stronę przeglądarki. <br />
-                        Jeśli to nie pomoże, postaramy się jak najszybciej naprawić błąd.
-                   </StyledError>
-                ) : (
-                  <>
-      <StyledFieldset>
-        <Legend>Kalkulator walut</Legend>
-        <Clock />
-        <p>
-          <label>
-            {" "}
-            <Label>Kwota:</Label>
-            <Field
-              value={amount}
-              onChange={({ target }) => setAmount(target.value)}
-              required
-              type="number"
-              min="1"
-              step="any"
-              placeholder="PLN"
-            />
-          </label>
-        </p>
-        <p>
-          <label>
-            {" "}
-            <Label>Waluta:</Label>
-            <Field
-              as="select"
-              value={currency}
-              onChange={({ target }) => setCurrency(target.value)}
-            >
-              {Object.keys(ratesData.rates).map((currency) => (
-                <option 
-                 key={currency} 
-                 value={currency}>
-                 {currency}
-                </option>
-              ))}
-            </Field>
-          </label>
-        </p>
-      </StyledFieldset>    
-      <StyledButton>Przelicz</StyledButton>
-      <Result result={result} />
-      <Info>
-                            Kursy walut aktualne na dzień: {ratesData.date}
-                        </Info>
-                    </>
-                )
-                }
+      {ratesData.state === "loading" ? (
+        <Loading>
+          Uno momento por favor... <br /> Właśnie ładuję kursy z Europejskiego
+          Banku Centralnego...
+        </Loading>
+      ) : ratesData.state === "error" ? (
+        <StyledError>
+          Ups... coś poszło nie tak 😐 <br />
+          Sprawdź czy masz połączenie z internetem i odśwież stronę
+          przeglądarki. <br />
+          Jeśli to nie pomoże, postaramy się jak najszybciej naprawić błąd.
+        </StyledError>
+      ) : (
+        <>
+          <StyledFieldset>
+            <Legend>Kalkulator walut</Legend>
+            <Clock />
+            <p>
+              <label>
+                {" "}
+                <Label>Kwota:</Label>
+                <Field
+                  value={amount}
+                  onChange={({ target }) => setAmount(target.value)}
+                  required
+                  type="number"
+                  min="1"
+                  step="any"
+                  placeholder="PLN"
+                />
+              </label>
+            </p>
+            <p>
+              <label>
+                {" "}
+                <Label>Waluta:</Label>
+                <Field
+                  as="select"
+                  value={currency}
+                  onChange={({ target }) => setCurrency(target.value)}
+                >
+                  {Object.keys(ratesData.rates).map((currency) => (
+                    <option key={currency} value={currency}>
+                      {currency}
+                    </option>
+                  ))}
+                </Field>
+              </label>
+            </p>
+          </StyledFieldset>
+          <StyledButton>Przelicz</StyledButton>
+          <Result result={result} />
+          <Info>Kursy walut aktualne na dzień: {ratesData.date}</Info>
+        </>
+      )}
     </form>
   );
 };
